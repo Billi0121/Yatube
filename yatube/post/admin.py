87812b539c73e016ed6d28f2c69b72b6fcf30488
Bilol desktop <gafarovbilol2009@gmail.com>
2025-03-22 17:22:01 +0500

    editing admin.py

--- yatube/post/admin.py
+++ yatube/post/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from post.models import Post
 
 # Register your models here.
-admin.site.register(Post)
+class AdminPost(admin.ModelAdmin):
+     # Перечисляем поля, которые должны отображаться в админке
+    list_display = ('text', 'pub_date', 'author') 
+    # Добавляем интерфейс для поиска по тексту постов
+    search_fields = ('text',) 
+    # Добавляем возможность фильтрации по дате
+    list_filter = ('pub_date',)
+    avaible_list_value = 'Pusto'
+
+admin.site.register(Post, AdminPost)
