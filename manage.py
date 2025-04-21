9322e74dffc49b3eb96aaa86652e27d393f2ee69
Billi0121 <gafarovbilol2009@gmail.com>
2025-04-21 17:38:26 +0500

    for saveti

--- manage.py
+++ manage.py
@@ -3,14 +3,14 @@ global_variable = 'Hello'
 def some_fun(passed_variable):
     local_variable = 'Andrey'
 
-    def local_fun_variable():
+    def local_fun_variable(last_variable):
         locla_inside_variable = 'DOING'
         return(f'{global_variable} '
                f'{local_variable} '
                f'{passed_variable} '
-               f'{locla_inside_variable}')
+               f'{locla_inside_variable}'
+               f'{last_variable}')
     return local_fun_variable
 
-some = some_fun('HOw ARE YOU')
-print(some())
 
+some = some_fun('HOw ARE YOU')
