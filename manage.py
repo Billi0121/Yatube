953e78ccdb6719d48c15a491b068b6c129ccea92
Bilol desktop <gafarovbilol2009@gmail.com>
2025-04-22 13:31:50 +0500

    pycahe

---
+++ manage.py
@@ -0,0 +1,7 @@
+def what_my_name():
+    """Это docstring функции what_my_name()."""
+    
+        
+print(f'Имя функции: {what_my_name.__name__}')
+print(f'Имя Достринга: {what_my_name.__doc__}')
+help(what_my_name)
