d0aff98704890445734fca4451cefc4f566329d0
Billi0121 <gafarovbilol2009@gmail.com>
2025-04-30 17:59:59 +0500

    transfering

--- yatube/about/views.py
+++ yatube/about/views.py
@@ -1,15 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+# from django.urls import 
 from django.views.generic import TemplateView
 from django.contrib.auth.models import User
 from posts import *
 
-# Create your views here.
-def only_outhorizade(funn):
-    def check_user(request, *args, **kwargs):
-        if request.user.is_authenticated:
-            return render(request, **args, **kwargs)
-        return redirect('auth/login')
-    return check_user
+# def only_user_log(func):
+#     def check_user(request, **kwargs):
+#         if request.user.is_authenticated:
+#             return render(request, **kwargs)
+#         return redirect('/auth/login/')
+#     return check_user
+
+
 
 class me(TemplateView):
     template_name = 'about/me.html'
