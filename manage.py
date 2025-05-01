15002a8991e9a922f938407311f6a1a6e4a38ac4
Bilol desktop <gafarovbilol2009@gmail.com>
2025-05-01 18:11:16 +0500

    transfering

--- manage.py
+++ manage.py
@@ -1,20 +1,54 @@
-def name_quotes(name):
-    quotes = {
-    'Darth Warder': 'Im your father',
-    'Anna': 'Whats hepennig',
-    'Lil nas x': 'Im gonna take my hourse',
-    'Bella' : 'Im barbi gir'
-    }
+def test_invert(invertaser):
+    """Тест алогритма инвертирования строки."""
+    print(f'{test_invert.__doc__}')
+    print(f'Тестирование алгоритма {invertaser.__name__}')
 
-    return quotes.get(name, 'He is not viral')
+    source = 'Попугай'
+    inversed = 'йагупоП'
 
-assert name_quotes('Anna') == 'Whats hepennig',(
-    'Its not workly corectly'
-)
-   
-assert name_quotes('Bella') == 'Im barbi girl',(
-    'Its not workly corectly'
-)
+    assert invertaser(source) == inversed, (
+        f'Алгоритм в {invertaser.__name__} работает неправильно '
+        f'со строкой "{source}"'
+    )
 
-print(name_quotes('Anna'))
+    source = ''
+    inversed = ''
 
+    assert invertaser(source) == inversed, (
+        f'Алгоритм {invertaser.__name__} работает неправильно с пустой строкой'
+    )
+
+    print(f'Тест для {invertaser.__name__} пройден успешно')
+
+
+# Ниже - несколько функций, инвертирующих строку.
+# Их и будем тестировать.
+def recursion_invertor(text):
+    """Инвертирует строчку рекурсивно."""
+    if len(text) == 0:
+        return text
+    return recursion_invertor(text[1:]) + text[0]
+
+
+def slice_invertor(text):
+    """Инвертирует строчку срезом."""
+    return text[::-1]
+
+
+def iterator_invertor(text):
+    """Инвертирует строчку обратной итерацией."""
+    return ''.join(reversed(text))
+
+
+def reverselist_invertor(text):
+    """Инвертирует строчку переворотом списка."""
+    inversed_list = list(text)
+    inversed_list.reverse()
+    return ''.join(inversed_list)
+
+
+# Вызов тестирующей функции для каждой функции-инвертора
+test_invert(recursion_invertor)
+test_invert(slice_invertor)
+test_invert(iterator_invertor)
+test_invert(reverselist_invertor) 
