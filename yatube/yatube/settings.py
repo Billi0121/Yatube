d65ff98e94871bf6cc3d96b6157c8cb84a8bf5f9
Billi0121 <gafarovbilol2009@gmail.com>
2025-07-22 16:10:51 +0500

    DD

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -111,11 +111,11 @@ CACHES = {
 DATABASES = {
     'default': {    
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'mydb',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': '127.0.0.1',
-        'PORT': '5432'        
+        'NAME': os.environ.get("DB_NAME"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("DB_HOST"),  # важно
+        'PORT': os.environ.get("DB_PORT", "5432"),    
     }
 }
 
